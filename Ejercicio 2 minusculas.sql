32d2241b5865c54cea7d12be414228db54712e3c
MelissaAvl09 <161497143+MelissaAvl09@users.noreply.github.com>
2024-10-14 21:16:23 -0600

    Ejercici 2 Guia 1

---
+++ Ejercicio 2 minusculas.sql
@@ -0,0 +1,20 @@
+USE biblioteca;
+GO
+
+-- Crear o alterar la función para convertir texto a minúsculas
+CREATE OR ALTER FUNCTION dbo.textoMinuscula(@Texto NVARCHAR(500))
+RETURNS NVARCHAR(500)
+AS
+BEGIN
+    RETURN LOWER(@Texto);
+END;
+GO
+
+
+-- Consultar los libros y sus estados en minúsculas
+SELECT 
+    dbo.textoMinuscula(l.libro) AS titulo_libro_minuscula,
+    dbo.textoMinuscula(l.estado) AS estado_minuscula
+FROM 
+    dbo.libros l;
+
