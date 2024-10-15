9cc44c2244d0e0a0ffb9eb028b4a50633ffe0d7d
LuisQuevdo <160786241+LuisQuevdo@users.noreply.github.com>
2024-10-14 21:17:09 -0600

    Ejercicio 1 Guia1

---
+++ Ejercicio 1.sql
@@ -0,0 +1,18 @@
+--Ejercicio 1: Función Escalar para Calcular la Edad de un Empleado
+
+create function fn_CalcularEdad (@fechaNacimiento DATE)
+returns INT
+as
+begin
+    return DATEDIFF(YEAR, @fechaNacimiento, GETDATE()) - 
+           case 
+               when MONTH(@fechaNacimiento) > MONTH(GETDATE()) OR 
+                    (MONTH(@fechaNacimiento) = MONTH(GETDATE()) AND DAY(@fechaNacimiento) > DAY(GETDATE())) 
+               then 1 
+               else 0 
+           end;
+end;
+go
+
+-- Ejemplo de uso:
+select dbo.fn_CalcularEdad('1990-02-02') as Edad;
